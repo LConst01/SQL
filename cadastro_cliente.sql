f424829ca0b2abe95d5a5099036267b402d6b633
LConst01 <cunhalucca26@gmail.com>
2025-09-16 16:36:10 -0300

    Add files via upload

--- cadastro_cliente.sql
+++ cadastro_cliente.sql
@@ -24,3 +24,34 @@ VALUES
     (4, 'Roberta', 'F', 5000.00, 564322),
     (5, 'Renata', 'F', 3000.00, 654321),
     (6, 'Giovanna', 'F', 3500.00, 56421);
+    
+ALTER TABLE Funcionario
+MODIFY COLUMN Data_Cadast_Func DATE NOT NULL;
+    
+INSERT INTO Funcionario (Nome_Func, Genero_Func, Sal_Func, Data_Cadast_Func)
+VALUES 
+	('Salvador', 'M', 7000.00, '1995-01-03'),
+    ('Antônio', 'M', 6000.00, '1998-05-06'),
+    ('Antonieta', 'F', 6500.00, '1998-08-07'),
+    ('Eduardo', 'M', 4000.00, '2000-08-06'),
+    ('Leny', 'F', 3000.00, '2001-08-06');
+/*Com base nas tabelas criadas anteriormente, escreva uma consulta que exiba o tipo se é 'cliente' ou 'funcionário', o nome, o gênero, a data de cadastro e o RG*/
+
+SELECT 
+	'Cliente' AS Tipo,
+    Nome_Cli AS Nome,
+    Genero_Cli AS Genero,
+    NULL AS Data_cadastro, 
+    RG_Cli AS RG
+FROM Cliente
+
+UNION ALL
+
+SELECT 
+	'Funcionario' AS Tipo,
+    Nome_Func AS Nome,
+    Genero_Func AS Genero,
+    DATE_FORMAT(Data_Cadast_Func, '%d/%m/%Y') AS Data_Cadastro, 
+	NULL AS RG
+FROM Funcionario;
+    
